be0e210af5988230878aa65a8744c5617a733e6e
Ernesto Ramos <ernesto@switcherstudio.com>
2022-01-25 23:57:41 -0500

    Created a simple console app that can read names from a CSV file, add them to a List<Person>, and prompt the user to add more people to the list.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using CsvHelper;
 
 namespace ConsoleApp1
 {
@@ -6,7 +11,60 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello dudes!");
+            List<Person> people = new List<Person>();
+
+            using (var reader = new StreamReader(@"data/people.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                people = csv.GetRecords<Person>().ToList();
+            }
+
+
+            string userInput = "";
+
+            do
+            {
+                AddPerson(people);
+                Console.WriteLine("To exit, type \"exit\" or just hit enter to add another person. ");
+                userInput = Console.ReadLine();
+            } while (userInput != "exit");
+
+
+            people.ForEach((person) => 
+            {
+                Console.WriteLine(person.FullName);
+            });
+        }
+
+        static void AddPerson(List<Person> people)
+        {
+            Console.WriteLine("Please enter a first name:");
+            var firstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter a last name:");
+            var lastName = Console.ReadLine();
+
+            Person person = new Person()
+            {
+                FirstName = firstName,
+                LastName = lastName
+            };
+
+            people.Add(person);
+        }
+
+    }
+
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
         }
     }
 }
